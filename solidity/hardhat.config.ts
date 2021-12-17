84ef58281187a1929c7864f5d0a003c0b01e241a
Jonatas Martins <jonatas.pvt@gmail.com>
2021-12-16 22:13:31 -0300

    add kovan deploy network

--- solidity/hardhat.config.ts
+++ solidity/hardhat.config.ts
@@ -9,8 +9,6 @@ import "solidity-coverage";
 
 dotenv.config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
@@ -19,26 +17,16 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
+    kovan: {
+      url: process.env.KOVAN_URL || "",
       accounts:
         process.env.DEPLOY_PRIVATE_KEY !== undefined
           ? [process.env.DEPLOY_PRIVATE_KEY]
           : [],
     },
-  },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
   },
 };
 
